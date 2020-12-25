75a191b7827d1d6e9f36a4ab8a00b7e126c334f1
Mr7ev3n <76255373+Mr7ev3n@users.noreply.github.com>
2020-12-25 22:08:22 +0700

    Update CS-DORKERv3.1.php

--- CS-DORKERv3.1.php
+++ CS-DORKERv3.1.php
@@ -26,7 +26,21 @@ function salam(){echo"
 \e[36m         \/____/                   \/____/       \e[93m +        \e[97m~~ \e[35mINDONESIAN H4X0R LITE \e[97m~~            \e[93m+
 ";
 }
-function Curl ($url, $post = 0, $headers = 0, $proxy = 0){
+function get_string($string, $start, $end){
+        $str = explode($start, $string);
+        $str = explode($end, $str[1]);
+        return $str[0];
+    }
+function parse($link){
+    $parse = parse_url($link);
+    return $parse['scheme'] . "://" . $parse['host'] . "/";
+}
+function saved($nama, $data){
+		$myfile = fopen($nama, "a");
+		fwrite($myfile, $data);
+		fclose($myfile);
+}
+function Curll ($url, $post = 0, $headers = 0, $proxy = 0){
         $ch = curl_init();
         $options = array(
             CURLOPT_URL => $url,
@@ -34,6 +48,7 @@ function Curl ($url, $post = 0, $headers = 0, $proxy = 0){
             CURLOPT_SSL_VERIFYPEER => 0,
             CURLOPT_SSL_VERIFYHOST => 0,
             CURLOPT_FOLLOWLOCATION => 1,
+			CURLOPT_CONNECTTIMEOUT => 3,
             CURLOPT_HEADER         => 1,
 			CURLOPT_TIMEOUT        => 30,
         );
@@ -54,39 +69,35 @@ function Curl ($url, $post = 0, $headers = 0, $proxy = 0){
         if(!$httpcode) return "Curl Error : ".curl_error($ch); else{
             $header = substr($response, 0, curl_getinfo($ch, CURLINFO_HEADER_SIZE));
             $body = substr($response, curl_getinfo($ch, CURLINFO_HEADER_SIZE));
-            curl_close($ch);
+            //curl_close($ch);
             return array($header, $body);
         }
 }
-function get_string($string, $start, $end){
-        $str = explode($start, $string);
-        $str = explode($end, $str[1]);
-        return $str[0];
-    }
-function parse($link){
-    $parse = parse_url($link);
-    return $parse['scheme'] . "://" . $parse['host'] . "/";
-}
-function saved($nama, $data){
-		$myfile = fopen($nama, "a");
-		fwrite($myfile, $data);
-		fclose($myfile);
+function Curl ($url){
+        $options = array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_SSL_VERIFYPEER => 0,
+            CURLOPT_SSL_VERIFYHOST => 0,
+            CURLOPT_FOLLOWLOCATION => 1,
+			CURLOPT_CONNECTTIMEOUT => 3,
+			CURLOPT_TIMEOUT 	   => 30,
+            CURLOPT_HEADER         => 1,
+        );
+        $ch = curl_init();
+		curl_setopt_array($ch, $options);
+		return curl_exec($ch);
+		//curl_close($ch);
 }
 function ask($dork){
     for ($i = 1;$i <= 201;$i+= 1) {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://www.ask.com/web?q=".rawurlencode($dork)."&qsrc=998&page=".$i);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-		$data = curl_exec($ch);
+		$url = "https://www.ask.com/web?q=".rawurlencode($dork)."&qsrc=998&page=".$i;
+		$data = Curl($url);
 		preg_match_all('#<p class="PartialSearchResults-item-url">(.*?)</p>#si', $data, $findlink);		
 		if($findlink[1] != NULL){
 			foreach ($findlink[1] as $link){
 				$links = "http://$link";
-				saved("ask_dump.txt","$links\n");
+				//saved("ask_dump.txt","$links\n");
 				echo"\e[97m[\e[34mask\e[97m][\e[38;5;208mpage\e[97m:\e[92m$i\e[97m]\n";
 				$allLinks[] = $links;
 			}
@@ -96,18 +107,12 @@ return array_unique(array_map("parse",$allLinks));
 }
 function bing($dork){
     for ($i = 1;$i <= 201;$i+= 10) {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://www.bing.com/search?q=".rawurlencode($dork)."&first=".$i."&FORM=PERE");
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-		$data = curl_exec($ch);
-        preg_match_all('#<h2><a href="(.*?)" h="ID#si', $data, $links);
+        $url = "https://www.bing.com/search?q=".rawurlencode($dork)."&first=".$i."&FORM=PERE";
+		$data = Curl($url);
+		preg_match_all('#<h2><a href="(.*?)" h="ID#si', $data, $links);
         if($links[1] != NULL){
 			foreach ($links[1] as $link) {
-			saved("bing_dump.txt","$link\n");
+			//saved("bing_dump.txt","$link\n");
 			echo"\e[97m[\e[35mbing\e[97m][\e[38;5;208mpage\e[97m:\e[92m$i\e[97m]\n";
             $allLinks[] = $link; 
 			}
@@ -121,16 +126,16 @@ $i = 0;
 		$headers = array();
         $headers[] = 'Referer: https://cse.google.com/cse?cx=partner-pub-2698861478625135:3033704849';
         $headers[] = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.'.rand(0000, 3333).'.169 Safari/537.36';
-		$getInfo = Curl("https://cse.google.com/cse.js?hpg=1&cx=partner-pub-2698861478625135:3033704849", 0, $headers);
+		$getInfo = Curll("https://cse.google.com/cse.js?hpg=1&cx=partner-pub-2698861478625135:3033704849", 0, $headers);
         preg_match('#"cse_token": "(.*?)"#si', $getInfo[1], $cseToken);
         $url = "https://cse.google.com/cse/element/v1?rsz=filtered_cse&num=10&&start=".$i."&hl=en&source=gcsc&gss=.com&cselibv=921554e23151c152&cx=partner-pub-2698861478625135:3033704849&q=".rawurlencode($dork)."&safe=off&cse_tok=".$cseToken[1]."&exp=csqr,cc&callback=google.search.cse.api16950";
-		$json = Curl($url, 0, $headers);
+		$json = Curll($url, 0, $headers);
         preg_match_all('#"clicktrackUrl": "(.*?)"#si', $json[1], $trackUrl);
 		$i = $i+10;
 		if($trackUrl[1] != NULL){
 			foreach($trackUrl[1] as $index => $key){
 				$jem1 = urldecode(get_string($key, '&q=', '&sa'));
-				saved("google_dump.txt","$jem1\n");
+				//saved("google_dump.txt","$jem1\n");
 				echo"\e[97m[\e[36mgoogle\e[97m][\e[38;5;208mpage\e[97m:\e[92m$i\e[97m]\n";
 				$allLinks[] = urldecode(get_string($key, '&q=', '&sa'));
 			}
@@ -139,19 +144,13 @@ $i = 0;
 return array_unique(array_map("parse",$allLinks));
 }
 function yahoo($dork){
-    for ($i = 1;$i <= 1001;$i+= 10) {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://search.yahoo.com/search?q=".rawurlencode($dork)."&b=".$i);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-		$data = curl_exec($ch);
-        preg_match_all('#href="(.*?)" referrerpolicy="origin"#si', $data, $links);
+    for ($i = 1;$i <= 13;$i+= 10) {
+        $url = "https://search.yahoo.com/search?q=".rawurlencode($dork)."&b=".$i;
+		$data = Curl($url);
+		preg_match_all('#href="(.*?)" referrerpolicy="origin"#si', $data, $links);
         if($links[1] != NULL){
 			foreach ($links[1] as $link) { 
-			saved("yahoo_dumper.txt","$link\n");
+			//saved("yahoo_dumper.txt","$link\n");
 			echo"\e[97m[\e[93myahoo\e[97m][\e[38;5;208mpage\e[97m:\e[92m$i\e[97m]\n";
             $allLinks[] = $link; 
 			}
@@ -161,18 +160,12 @@ return array_unique(array_map("parse",$allLinks));
 }
 function wow($dork){
     for ($i = 0;$i <=1001;$i+=7) {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://www.wow.com/search?q=".rawurlencode($dork)."&b=".$i);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-		$data = curl_exec($ch);
-        preg_match_all('#RU=(.*?)RK#si', $data, $links);
+        $url = "https://www.wow.com/search?q=".rawurlencode($dork)."&b=".$i;
+		$data = Curl($url);
+		preg_match_all('#RU=(.*?)RK#si', $data, $links);
         if($links[1] != NULL){
 			foreach ($links[1] as $link) {
-			saved("wow_dumper.txt","$link\n");
+			//saved("yahoo_dumper.txt","$link\n");
 			echo"\e[97m[\e[37mwow\e[97m][\e[38;5;208mpage\e[97m:\e[92m$i\e[97m]\n";
             $allLinks[] = urldecode($link);
 			}
@@ -182,18 +175,12 @@ return array_unique(array_map("parse",$allLinks));
 }
 function aol($dork){
     for ($i = 1;$i <=1001;$i+=10) {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://search.aol.com/aol/search?q=".rawurlencode($dork)."&b=".$i);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-		$data = curl_exec($ch);
-        preg_match_all('#" href="(.*?)"#si', $data, $links);
+        $url = "https://search.aol.com/aol/search?q=".rawurlencode($dork)."&b=".$i;
+		$data = Curl($url);
+		preg_match_all('#" href="(.*?)"#si', $data, $links);
         if($links[1] != NULL){
 			foreach ($links[1] as $link) {
-			saved("aol_dumper.txt","$link\n");
+			//saved("aol_dumper.txt","$link\n");
 			echo"\e[97m[\e[1maol\e[97m][\e[38;5;208mpage\e[97m:\e[92m$i\e[97m]\n";
             $allLinks[] = urldecode($link);
 			}
@@ -204,15 +191,15 @@ return array_unique(array_map("parse",$allLinks));
 function duck($dork){
     for ($i = 0;$i <=50;$i+=10) {
 		$headers = array();
-        $headers[] = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.'.rand(0000, 3333).'.169 Safari/537.36';
-		$getInfo = Curl("https://duckduckgo.com/?q=".rawurlencode($dork), 0, $headers);
+        $headers[] = 'User-Agent: DuckDuckBot/1.0; (+http://duckduckgo.com/duckduckbot.html)';
+		$getInfo = Curll("https://duckduckgo.com/?q=".rawurlencode($dork), 0, $headers);
 		preg_match('#vqd=(.*?)&#si', $getInfo[1], $vqd);
-		$json = Curl("https://links.duckduckgo.com/d.js?q=".rawurlencode($dork)."&s=".$i."&vqd=".$vqd[1], 0, $headers);
+		$json = Curll("https://links.duckduckgo.com/d.js?q=".rawurlencode($dork)."&s=".$i."&vqd=".$vqd[1], 0, $headers);
         preg_match_all('#"http(.*?)"#si', $json[1], $trackUrl);
 		if($trackUrl[1] != NULL){
 			foreach($trackUrl[1] as $jem){
 				$jem1 = "http$jem";
-				saved("DuckDugo_dump.txt","$jem1\n");
+				//saved("DuckDugo_dump.txt","$jem1\n");
 				echo"\e[97m[\e[93mDuckDugo\e[97m][\e[38;5;208mpage\e[97m:\e[92m$i\e[97m]\n";
 				$allLinks[] = $jem1;
 			}
@@ -222,19 +209,13 @@ return array_unique(array_map("parse",$allLinks));
 }
 function izito($dork){
     for ($i = 1;$i <=5;$i+=1) {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://www.izito.ws/ws?q=".rawurlencode($dork)."&pg=".$i);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-		$data = curl_exec($ch);
-        preg_match_all('#"http(.*?)" rel="#si', $data, $links);
+		$url = "https://www.izito.ws/ws?q=".rawurlencode($dork)."&pg=".$i;
+		$data = Curl($url);
+		preg_match_all('#"http(.*?)" rel="#si', $data, $links);
         if($links[1] != NULL){
 			foreach ($links[1] as $linka) {
 			$link ="http$linka";
-			saved("izito_dumper.txt","$link\n");
+			//saved("izito_dumper.txt","$link\n");
 			echo"\e[97m[\e[35mizito\e[97m][\e[38;5;208mpage\e[97m:\e[92m$i\e[97m]\n";
             $allLinks[] = urldecode($link);
 			}
@@ -271,11 +252,11 @@ $done = implode("\n", array_unique($garap));
 saved("result.txt","$done\n");
 $c = count(file("result.txt"));
 $diff = $first->diff($second);
-$elapse = $diff->format('%i,%s');Mr7ev3n
+$elapse = $diff->format('%i,%s');
 echo "$done
 \e[93m++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 \e[93m+ \e[93m@author    \e[93m: \e[93mMr.7ev3n                   \e[93m+ 
-\e[93m+ \e[91m@github    \e[93m: \e[91mhttps://github.com/Mr7ev3n \e[93m+ \e[97m[\e[38;5;208mSite \e[97m: \e[38;5;208m$dorkr2\e[97m]
+\e[93m+ \e[91m@github    \e[93m: \e[91mhttps://github.com/N93NT0T \e[93m+ \e[97m[\e[38;5;208mSite \e[97m: \e[38;5;208m$dorkr2\e[97m]
 \e[93m+ \e[92m@youtube   \e[93m: \e[92mbit.ly/n93nt0t             \e[93m+ \e[97m[ \e[36mToTaL \e[97m: \e[36m$c \e[97m]
 \e[93m+ \e[35m@twitter   \e[93m: \e[35mbit.ly/t_ng93nt0t          \e[93m+ \e[97m[ \e[92m$elapse Second \e[97m]
 \e[93m+ \e[34m@instagram \e[93m: \e[34mbit.ly/i_n93nt0t           \e[93m+ \e[97m[ \e[93m$c_e\e[97m/\e[93m$c_d \e[97m]
@@ -307,7 +288,7 @@ $elapsed = $diff->format('%H:%i:%s');salam();
 echo"                              \e[92mDONE!                                                   
 \e[93m++++++++++++++++++++++++++++++++++++++++++++++      +        +               +             +            +
 \e[93m+ \e[93m@author       \e[93m: \e[93mMr.7ev3n                   \e[93m  +     +        +           +          +
-\e[93m+ \e[91m@github       \e[93m: \e[91mhttps://github.com/Mr7ev3n \e[93m             +        +            +      +
+\e[93m+ \e[91m@github       \e[93m: \e[91mhttps://github.com/N93NT0T \e[93m             +        +            +      +
 \e[93m+ \e[92m@youtube      \e[93m: \e[92mbit.ly/n93nt0t             \e[93m +    +                               +
 \e[93m+ \e[35m@twitter      \e[93m: \e[35mbit.ly/t_ng93nt0t          \e[93m          +     +           +
 \e[93m+ \e[34m@instagram    \e[93m: \e[34mbit.ly/i_n93nt0t           \e[93m   +                +                   
